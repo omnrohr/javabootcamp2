ef1773e5b01865848e55cb585a8e73c03a119ad3
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-01 19:26:21 +0200

    Streem after adding streem

--- src/com/company/streemStarter/Filter.java
+++ src/com/company/streemStarter/Filter.java
@@ -13,17 +13,20 @@ public class Filter {
         prices.add(10.99);
         prices.add(15.99);
 
-        filterLowPrices();
+        prices.stream()
+                        .filter((price) -> price<5)
+                        .forEach((price)-> System.out.println(price));
+//        filterLowPrices();
 
     }
 
-    public static void filterLowPrices() {
-        System.out.println("\n\nLOW PRICES");
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices.get(i) < 5) {
-                System.out.println(prices.get(i)); 
-            }
-        }
-    }
+//    public static void filterLowPrices() {
+//        System.out.println("\n\nLOW PRICES");
+//        for (int i = 0; i < prices.size(); i++) {
+//            if (prices.get(i) < 5) {
+//                System.out.println(prices.get(i));
+//            }
+//        }
+//    }
 
 }
