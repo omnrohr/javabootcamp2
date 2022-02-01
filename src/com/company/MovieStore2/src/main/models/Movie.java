bf77d4c72efcd3737730a708fbac3aca96a50be4
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-01 20:40:54 +0200

    movies store tests final

--- src/com/company/MovieStore2/src/main/models/Movie.java
+++ src/com/company/MovieStore2/src/main/models/Movie.java
@@ -38,7 +38,7 @@ public class Movie {
     public String getName() {
         return name;
     }
-    
+
     public String getFormat() {
         return format;
     }
@@ -96,11 +96,30 @@ public class Movie {
 
     public String toString() {
         return "\t Name: " + this.name + "\n" +
-               "\t Format: " + this.format + "\n" +
-               "\t Rating: " + this.rating + "\n" +
-               "\t Selling Price: " + this.sellingPrice + "\n" +
-               "\t Rental Price: " + this.rentalPrice + "\n" +
-               "\t Availability: " + (this.isAvailable ? "in-stock" : "rented") + "\n";
+                "\t Format: " + this.format + "\n" +
+                "\t Rating: " + this.rating + "\n" +
+                "\t Selling Price: " + this.sellingPrice + "\n" +
+                "\t Rental Price: " + this.rentalPrice + "\n" +
+                "\t Availability: " + (this.isAvailable ? "in-stock" : "rented") + "\n";
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof Movie)) {
+            return false;
+        }
+
+        Movie movie = (Movie)obj;
+        return this.name.equals(movie.name) &&
+                this.format.equals(movie.format) &&
+                this.rating == movie.rating &&
+                this.sellingPrice == movie.sellingPrice &&
+                this.rentalPrice == movie.rentalPrice &&
+                this.isAvailable == movie.isAvailable;
+
     }
 
 }
--- src/com/company/MovieStore2/src/test/StoreTest.java
+++ src/com/company/MovieStore2/src/test/StoreTest.java
@@ -1,9 +1,50 @@
 package com.company.MovieStore2.src.test;
 
-import src.main.models.Store;
+
+import com.company.MovieStore2.src.main.models.Movie;
+import com.company.MovieStore2.src.main.models.Store;
+import org.junit.Before;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class StoreTest {
 
     Store store;
 
+    @Before
+    public void setup() {
+        store = new Store();
+        store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+        store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+
+    }
+
+    @Test
+    public void movieAdded() {
+        assertTrue(store.contains(new Movie("The Godfather", "Blue-Ray", 9.1)));
+    }
+
+    @Test
+    public void sellMovieTest() {
+        store.sellMovie("The Shawshank Redemption");
+        assertFalse(store.contains(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2)));
+    }
+
+    @Test
+    public void rentMovieTest() {
+        store.rentMovie("The Godfather");
+        assertFalse(store.getMovie(1).isAvailable());
+    }
+
+    @Test
+    public void returnMovieTest() {
+        store.rentMovie("The Godfather");
+        store.returnMovie("The Godfather");
+        assertTrue(store.getMovie(1).isAvailable());
+    }
+
+
 }
+
