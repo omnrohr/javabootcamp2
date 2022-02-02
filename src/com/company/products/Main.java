31acdcd85fdf24d8e9b0db7c682082d9eb10dc82
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-02 23:14:33 +0200

    get data from a file

--- src/com/company/products/Main.java
+++ src/com/company/products/Main.java
@@ -4,31 +4,23 @@ import com.company.products.models.Pants;
 import com.company.products.models.Product;
 import com.company.products.models.Shirt;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
   
     static final String FILE_NAME = "D:\\javabootcamp2\\exceptions\\src\\com\\company\\products\\products.txt";
 
     public static void main(String[] args) {
-        Product[] products = new Product[] {
-                new Pants(32, 24.99, "Blue", "JAVA KLEIN"),
-                new Shirt(25, 22.99, "Black", "CHANEL"),
-                new Pants(34, 104.99, "Red", "JANGLER"),
-                new Shirt(17, 13.99, "Orange", "GEORGE"),
-                new Pants(30, 119.99, "Grey", "FENDI"),
-                new Shirt(35, 34.99, "Blue", "ECKO"),
-                new Pants(30, 129.99, "Red", "VERSACE"),
-                new Shirt(27, 22.99, "Beige", "ZARA"),
-                new Pants(29, 99.99, "Dark", "JANGLER"),
-                new Shirt(19, 19.99, "Red", "NIKE"),
-                new Pants(26, 24.99, "Indigo", "BELSTAFF"),
-                new Shirt(22, 29.99, "Blue", "ADIDAS"),
-                new Pants(34, 104.99, "Red", "JANGLER"),
-        };
+        try {
+        Product[] products = getData();
         Arrays.sort(products);
         printArray(products);
-
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void printArray(Product[] array) {
@@ -45,4 +37,20 @@ public class Main {
      * Inside the function:
      *   1. Loads the data from products.txt
      */
+
+    public static Product[] getData() throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream(FILE_NAME);
+        Scanner scan = new Scanner(fis);
+
+        Product[] products = new Product[18];
+
+        for (int i = 0; scan.hasNextLine(); i++) {
+            switch (scan.next()) {
+                case "SHIRT": products[i] = new Shirt(scan.nextInt(), scan.nextDouble(), scan.next(), scan.next()); break;
+                case "PANTS": products[i] = new Pants(scan.nextInt(), scan.nextDouble(), scan.next(), scan.next()); break;
+            }
+        }
+        scan.close();
+        return products;
+    }
 }
