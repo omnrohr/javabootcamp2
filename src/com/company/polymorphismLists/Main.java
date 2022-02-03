a144299f799e3d69b8a8c106515d1e0b1022aaea
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-03 09:42:50 +0200

    polymorphism-lists

---
+++ src/com/company/polymorphismLists/Main.java
@@ -0,0 +1,13 @@
+package com.company.polymorphismLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+
+    }
+}
